a0723e331a58c2eaab36916831afe397df4ef556
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-06-20 17:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let tbl = document.getElementById("sampleTable");
-
 function insert_Row() {
-  let x = document.createElement("tr");
-  x.innerHTML = `
-    <td>New Cell1</td>
-    <td>New Cell2</td>`;
+  let tbl = document.getElementById("sampleTable");
+  let x = tbl.insertRow(0); // Insert row at index 0 (top)
 
-  tbl.appendChild(x);
+  let cell1 = x.insertCell();
+  let cell2 = x.insertCell();
+
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
+
+// let tbl = document.getElementById("sampleTable");
+
+// function insert_Row() {
+//   let x = document.createElement("tr");
+//   x.innerHTML = `
+//     <td>New Cell1</td>
+//     <td>New Cell2</td>`;
+
+//   tbl.appendChild(x);
+// }
